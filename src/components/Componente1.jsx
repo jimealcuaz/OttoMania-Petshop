8eb2844d271515249f249ac9a68dd1c5b58a05b2
jimealcuaz <alcuazjimena@gmail.com>
2023-02-01 19:14:14 -0300

    primer entrega

---
+++ src/components/Componente1.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+/*FUNCION FLECHA QUE RETORNA DETERMINADO CONTENIDO*/
+
+/*EN LO SIGUIENTE ESTOY CREANDO UN COMPONENTE CON EL ATAJO RAFCE*/
+
+const Componente1 = () => {
+
+
+    return (
+        <div>
+            <h1>Petshop</h1>
+        </div>
+    )
+}
+
+export default Componente1
