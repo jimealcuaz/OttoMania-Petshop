b6d5cbd4be06223283c4b0871892edae1813095c
jimealcuaz <alcuazjimena@gmail.com>
2023-02-11 10:12:07 -0300

    contador

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,33 @@ import ItemCount from "./components/ItemCount";
 
 const App = () => {
 
-    return(
+  let productos = [
+    { id: 1, nombre: "producto1", precio: 1000 },
+    { id: 2, nombre: "producto2", precio: 2000 },
+    { id: 3, nombre: "producto3", precio: 3000 },
+  ]
+  const aplicarDescuento = new Promise((resolve, reject) => {
+    const descuento = false;
+    if (descuento) {
+      resolve("descuento aplicado");
+    }
+    else {
+      reject("no existen descuentos");
+    }
+  });
+
+  setTimeout(() => {
+    console.log("setTimeOut");
+  }, 2000);
+
+  console.log(aplicarDescuento);
+
+
+  return (
     <div>
-      <NavBar/>
-      <ItemListContainer greeting= "Todo para tu mascota en un solo lugar"/>
-      <ItemCount stock={5}/>
+      <NavBar />
+      <ItemListContainer greeting="Todo para tu mascota en un solo lugar" />
+      <ItemCount stock={5} />
     </div>
   );
 };
