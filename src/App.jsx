b3751c63a7a89188df224f9a1a370b5b4a5e93f9
jimealcuaz <alcuazjimena@gmail.com>
2023-02-11 11:34:04 -0300

    componentes2

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,15 @@
 import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
-import ItemCount from "./components/ItemCount";
+import Componente1 from "./components/Componente1";
 
 const App = () => {
-
-  let productos = [
-    { id: 1, nombre: "producto1", precio: 1000 },
-    { id: 2, nombre: "producto2", precio: 2000 },
-    { id: 3, nombre: "producto3", precio: 3000 },
-  ]
-  const aplicarDescuento = new Promise((resolve, reject) => {
-    const descuento = false;
-    if (descuento) {
-      resolve("descuento aplicado");
-    }
-    else {
-      reject("no existen descuentos");
-    }
-  });
-
-  setTimeout(() => {
-    console.log("setTimeOut");
-  }, 2000);
-
-  console.log(aplicarDescuento);
 
 
   return (
     <div>
       <NavBar />
       <ItemListContainer greeting="Todo para tu mascota en un solo lugar" />
-      <ItemCount stock={5} />
+      <Componente1 />
     </div>
   );
 };
