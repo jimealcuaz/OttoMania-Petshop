2f479814ad08f1a3bfc5843a321c1cf140896292
jimealcuaz <alcuazjimena@gmail.com>
2023-02-13 18:14:32 -0300

    clase6

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -9,7 +9,7 @@ const ItemList = () => {
         {id: 3, nombre: "Producto 3", precio: 3000, stock: 10},
         {id: 4, nombre: "Producto A", precio: 1500, stock: 7},
         {id: 5, nombre: "Producto B", precio: 2500, stock: 3},
-        {id: 1, nombre: "Producto C", precio: 3500, stock: 8},
+        {id: 6, nombre: "Producto C", precio: 3500, stock: 8},
     ];
   
     const getProductos = () => {
